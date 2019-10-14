53e681a8eed84ecc23c84689f6fb511130284a36
sanekkurt <36759595+sanekkurt@users.noreply.github.com>
2019-10-14 15:15:54 +0500

    4

--- src/WeatherWidget.tsx
+++ src/WeatherWidget.tsx
@@ -13,22 +13,37 @@ const fetchData = async (
   currency: string,
   setTemp: (t: number) => void,
   setPrevious: (t: number) => void,
-  setName: (t: string) => void
+  setName: (t: string) => void,
+  setError: (t: string) => void
 ) => {
   if (currency.length < 3) return;
-  // TODO выбрать любой api-сервис и заменить код запроса и обработки ответа
   // TODO обработка ошибок при выполнении запроса
   // TODO сделать индикатор выполнения запроса
 
   const url = `https://www.cbr-xml-daily.ru/daily_json.js`;
   const response = await fetch(url);
   const data = await response.json();
-  const temp = data["Valute"][currency]["Value"];
-  const previous = data["Valute"][currency]["Previous"];
-  const name = data["Valute"][currency]["Name"];
+  console.log(data);
+  var temp = 0;
+  var previous = 0;
+  var name = "-";
+  var error = undefined;
+  if (data["Valute"][currency] === undefined) {
+    temp = undefined;
+    previous = undefined;
+    name = undefined;
+    error = "Введите верную валюту";
+  } else {
+    temp = data["Valute"][currency]["Value"];
+    previous = data["Valute"][currency]["Previous"];
+    name = data["Valute"][currency]["Name"];
+    error = undefined;
+  }
+
   setTemp(temp);
   setPrevious(previous);
   setName(name);
+  setError(error);
 };
 
 const debouncedFetchData = debounce(fetchData, 500);
@@ -43,8 +58,10 @@ const WeatherWidget = () => {
 
   const [name, setName] = React.useState<string>(null);
 
+  const [error, setError] = React.useState<string>(null);
+
   React.useEffect(() => {
-    debouncedFetchData(currency, setTemp, setPrevious, setName);
+    debouncedFetchData(currency, setTemp, setPrevious, setName, setError);
   }, [currency]); // При изменении названия валюты вызывается функция-эффект
 
   return (
@@ -62,6 +79,8 @@ const WeatherWidget = () => {
         <Typography variant="h6">Старая цена: {previous}</Typography>
         <p />
         <Typography variant="h6">Имя валюты: {name}</Typography>
+        <p />
+        <Typography variant="h6">{error}</Typography>
       </CardContent>
     </Card>
   );
